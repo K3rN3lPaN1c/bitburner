e2150b84172166488b633748c956944840c417df
Norbert Nadler <norbert.nadler@webpont.com>
2021-12-23 13:20:51 +0100

    fixed downloadFiles.js so it doesn't kill itself,

--- src/ns2/downloadFiles.js
+++ src/ns2/downloadFiles.js
@@ -1,7 +1,22 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
-    await ns.wget("https://raw.githubusercontent.com/K3rN3lPaN1c/bitburner/feature/kuvee-ns-scripts/src/ns2/init.js", "init.js");
-    ns.killall("home");
-    ns.run("init.js");
+    //let baseUrl = "https://raw.githubusercontent.com/K3rN3lPaN1c/bitburner/feature/kuvee-ns-scripts/src/ns2/";
+    let baseUrl = "file:///C:/Bitburner/bitburner/src/ns2/";
+
+    await ns.wget(baseUrl + "init.js", "init.js");
+
+    killAllScriptsExceptThis(ns);
+    ns.exec("init.js", "home");
     ns.tail("init.js");
+}
+
+/** @param {import(".").NS } ns */
+function killAllScriptsExceptThis(ns) {
+    let runningScripts = ns.ps();
+    for (let i = 0 ; i < runningScripts.length ; i++) {
+        let scriptName = runningScripts[i].filename;
+        if (scriptName !== ns.getScriptName()) {
+            ns.kill(scriptName);
+        }
+    }
 }
